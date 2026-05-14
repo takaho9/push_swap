fa12dd2cc01904029660091d3898147a162b25e4
Weiran Zhou <wezhou@c1r2s2.42tokyo.jp>
2026-05-14 20:40:08 +0900

    0514 2040

---
+++ push_swap.h
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wezhou <wezhou@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/14 18:39:10 by wezhou            #+#    #+#             */
+/*   Updated: 2026/05/14 20:39:15 by wezhou           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+typedef struct s_node
+{
+	int	value;
+	int	index;
+	struct s_node *next;
+	struct s_node *prev;
+}	t_node;
+
+typedef struct s_ops
+{
+	int	sa;
+	int	sb;
+	int	ss;
+	int	pa;
+	int	pb;
+	int	ra;
+	int	rb;
+	int	rr;
+	int	rra;
+	int	rrb;
+	int	rrr;
+	int	total;
+}	t_ops;
+
+
+
+
+#endif
