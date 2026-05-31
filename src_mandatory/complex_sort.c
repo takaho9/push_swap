bc2d65d6c6067a970c647c710dce33cfc7384250
Takaho Takemura <ttakemur@c1r2s3.42tokyo.jp>
2026-05-31 15:07:27 +0900

    correct norm

--- src_mandatory/complex_sort.c
+++ src_mandatory/complex_sort.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   complex_sort.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wezhou <wezhou@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*   By: ttakemur <ttakemur@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/16 20:18:18 by ttakemur          #+#    #+#             */
-/*   Updated: 2026/05/27 14:53:03 by wezhou           ###   ########.fr       */
+/*   Updated: 2026/05/31 15:05:56 by ttakemur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,19 +25,26 @@ static size_t	max_bit(size_t max_index)
 	return (bits);
 }
 
+static int	check_a_complex(t_stack *a_stack, t_stack *b_stack, t_ops *ops)
+{
+	if (!a_stack || a_stack->size < 2)
+		return (ERROR);
+	if (a_stack -> size <= 5)
+	{
+		less_than_or_e_5(a_stack, b_stack, ops);
+		return (0);
+	}
+	return (1);
+}
+
 void	complex_sort(t_stack *a_stack, t_stack *b_stack, t_ops *ops)
 {
 	size_t	bits;
 	size_t	bit;
 	size_t	loop;
 
-	if (!a_stack || a_stack->size < 2)
+	if (check_a_complex(a_stack, b_stack, ops) <= 0)
 		return ;
-	if (a_stack -> size <= 5)
-	{
-		less_than_or_e_5(a_stack, b_stack, ops);
-		return ;
-	}
 	bits = max_bit(a_stack->size - 1);
 	bit = 0;
 	while (bit < bits)
